37d6a80cf223911c1a6f9ebd73a418f087be6239
Grayson Dubois <gmdub94@gmail.com>
2016-03-19 20:32:19 -0500

    Created pages for each tab. Made jQuer scripts for handling the displaying, adding, and removing of roommates

--- web/app/app.component.js
+++
@@ -1,8 +0,0 @@
-(function(app) {
-    app.AppComponent = ng.core.Component({
-        selector: 'my-app',
-        template: '<h1>Room8</h1>'
-    }).class({
-        constructor: function() {}
-    });
-}) (window.app || (window.app = {}));
