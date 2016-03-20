466909fbb7b18409a11765339ab491ab1a6a47b8
Grayson Dubois <gmdub94@gmail.com>
2016-03-19 21:44:29 -0500

    Added bills table view

--- web/js/main.js
+++ web/js/main.js
@@ -4,8 +4,16 @@ $(document).ready( function(){
     $("#roommate-list").append("<li class='list-group-item'>Brendan Fenton</li>");
     $("#roommate-list").append("<li class='list-group-item'>Regina Shelton</li>");
     $("#roommate-list-container").append("<span><input type='text' id='new-roommate-name' value='' placeholder='Enter New Roommate' style='width:20em;'></input>" +
-                                         "<button type='submit' class='pull-right btn btn-primary' id='btn-add-roommate'>Add Roommate</button>" +
-                                         "<button type='submit' class='pull-right btn btn-primary' id='btn-remove-roommate'>Remove Roommate</button></span>");
+                                         "<button type='submit' class='pull-right btn btn-primary' id='btn-add-roommate' style='margin:4px;'>Add Roommate</button>" +
+                                         "<button type='submit' class='pull-right btn btn-primary' id='btn-remove-roommate' style='margin:4px;'>Remove Roommate</button></span>");
+
+    $("#bills-table").append("<tr><td>Rent</td><td>$750</td></tr>");
+    $("#bills-table").append("<tr><td>Murfreesboro Electric</td><td>$90.73</td></tr>");
+    $("#bills-table").append("<tr><td>Murfreesboro Water and Sewer</td><td>$43.89</td></tr>");
+    $("#bills-list-container").append("<span><input type='text' id='new-bill-name' value='' placeholder='Enter Bill Description' style='width:20em;'></input>" +
+                                      "\t\t$<input type='number' id='new-bill-cost' value='0' placeholder='Enter Bill Amount' style='width:20em;'></input>" +
+                                         "<button type='submit' class='pull-right btn btn-primary' id='btn-add-bill' style='margin:4px;'>Add Bill</button>" +
+                                         "<button type='submit' class='pull-right btn btn-primary' id='btn-remove-bill' style='margin:4px;'>Remove Bill</button></span>");
 
     // Add a roommate when the add button is clicked
     $("#btn-add-roommate").click( function() {
@@ -29,11 +37,44 @@ $(document).ready( function(){
         }
     });
 
-
     // Enable selection of roommates
     $(".list-group li").click( function(){
         $(".list-group-item.active").removeClass("active");
         $(this).addClass("active");
     });
 
+
+    // Add a bill when the add button is clicked
+    /*$("#btn-add-bill").click( function() {
+
+        var validDesc = false;
+        var validCost = false;
+
+        if ( $("#new-bill-name").val() == NULL || $("#new-bill-name").val() == "" ) {
+            alert("Please insert a bill description");
+            validDesc = false;
+        } else {
+            validDesc = true;
+        }
+
+        if ( $("#new-bill-cost").val() ==  ) {
+            alert("Please insert a bill cost");
+            validCost = false;
+        } else {
+            validCost = true;
+        }
+
+        if ( validDesc && validCost ){
+            $("#bills-list").append("<li class='list-group-item'>" +
+                                        "<table><td><tr>" + $("#new-bill-name").val() + "</tr>" +
+                                        "<tr>$" + $("#new-bill-cost").val() + "</tr></td></table>" +
+                                    "</li>");
+
+            // Enable selection of roommates
+            $(".list-group li").click( function(){
+                $(".list-group-item.active").removeClass("active");
+                $(this).addClass("active");
+            });
+        }
+    });*/
 });
